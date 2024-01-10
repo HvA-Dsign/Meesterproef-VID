bc4984a0901d5e0b74ed37945154e1fe19dbf57d
HvA-Dsign <118122098+HvA-Dsign@users.noreply.github.com>
2024-01-10 15:10:57 +0100

    Versie 3 toegevoegd

---
+++ versie_3/website_notificaties/script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const video = document.getElementById('webcam')
+    const messageContainer = document.getElementById('messageContainer')
+    let audioNotif = new Audio('iphone_ding.mp3')
+
+    audioNotif.autoplay = false
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                video.srcObject = stream
+            })
+            .catch(function (error) {
+                console.error('Error accessing webcam:', error)
+            })
+    }
+
+    function checkOpacity() {
+        const section103 = document.querySelector('section:nth-of-type(103)')
+
+        if (section103 && window.getComputedStyle(section103).opacity === '1') {
+            messageContainer.style.display = 'none'
+            audioNotif.play()
+        }
+    }
+
+    // Controleer de opacity elke seconde
+    setInterval(checkOpacity, 500)
+
+    // Voorkom standaardgedrag van het notif-element (bijv. voorkom het herladen van de pagina)
+    const messageBtn = document.getElementById('notif')
+    messageBtn.addEventListener('click', function (event) {
+        event.preventDefault()
+    })
+})
